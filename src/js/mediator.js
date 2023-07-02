7a27b5852c1b9599dc2121895705659cb186a659
Damon <126731021+damon314159@users.noreply.github.com>
2023-07-02 05:33:46 +0100

    Add mediator object methods

---
+++ src/js/mediator.js
@@ -0,0 +1,15 @@
+const mediator = {
+  events: {},
+  subscribe: function(eventName, callback) {
+    this.events[eventName] ||= [];
+    this.events[eventName].push(callback);
+  },
+  unsubscribe: function(eventName, callback) {
+    this.events[eventName] &&= this.events[eventName].filter(f => f!==callback);
+  },
+  publish: function(eventName, data) {
+    this.events[eventName] && this.events[eventName].forEach(f=>{f(data);});
+  },
+};
+
+export default mediator;
